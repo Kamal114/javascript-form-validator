9aeac3b197eeb5aaf96b8342a1d667a50433a624
Kamal Deepak <kamaldsharma89@gmail.com>
2020-02-18 11:45:33 +0530

    Add Email validation function

--- js/main.js
+++ js/main.js
@@ -16,6 +16,15 @@ function showsuccess(input) {
   form_control.classList = "form-control success";
 }
 
+function checkEmail(input) {
+  const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  if (re.test(input.value.trim())) {
+    showsuccess(input);
+  } else {
+    showerror(input, "Email is not valid");
+  }
+}
+
 function checkRequired(inputArr) {
   inputArr.forEach(function(input) {
     if (input.value.trim() === "") {
@@ -48,4 +57,5 @@ form.addEventListener("submit", function(e) {
   checkRequired([username, email, password, password2]);
   checkLength(username, 3, 15);
   checkLength(password, 6, 25);
+  checkEmail(email);
 });
